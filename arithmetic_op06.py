0f5a9f71e69c1bdad82b7c4a1a1f4cf969f8b215
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-04 12:05:07 +0500

    06

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,9 @@
 #Create a variable called 'number' with data type int.
- 
+number=16
 #Divide the number by 2 and assign number.
- 
+answer=number%2
 #Find the fraction of the number and assing to a variable called 'answer'.
- 
+
 #Print the answer.
- 
+print(answer)
 #Print the number.
