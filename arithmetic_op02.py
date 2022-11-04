a8d58b0546bbe1d62631bb89002b0d496eb7d083
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-04 11:37:37 +0500

    02

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,7 @@
 #Create a varaible 'number' and assign it the value of 43.
-
+number=88
 #Create a variable 'answer' and assign it the sum of the number's digits.
-
+a=number//10
+b=number%10
 #Print answer.
+print(a+b)
