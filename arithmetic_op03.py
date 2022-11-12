18e3389aed354f3687efb0776cba0724ab64cd46
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-12 13:45:19 +0500

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,9 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-number=11
+number=51
 #Find the reverse of the number and assign it to a variable called 'answer'.
-x=number%10
-number//=10
-y=number%10
+answer=number%10*10 + number//10
 #Print the answer variable
-answer=x*10+y
 print(answer)
