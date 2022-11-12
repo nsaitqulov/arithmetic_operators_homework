d32bfeb494676ce0575d4b7ed8724b225462c224
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-12 13:55:50 +0500

    06

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,5 @@
 #Create a variable called 'number' and assign it the two-digit number
-number=51
+number=50
 #Find the reverse of the number and assign it to a variable called 'answer'.
 answer=number%10*10 + number//10
 #Print the answer variable
--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,10 +1,8 @@
 #Create a variable called 'number' with data type int.
-number=45
+number=54
 #Divide the number by 2 and assign number.
-x=number/2
+answer=number/2%1
 #Find the fraction of the number and assing to a variable called 'answer'.
-answer=x%1
 #Print the answer.
-print(answer)
 #Print the number.
-print(x)
+print(answer)
